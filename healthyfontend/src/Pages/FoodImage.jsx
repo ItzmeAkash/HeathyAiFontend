b56e66fa431509be1589c03400154640294f34b9
Akash Ps <itzmeakashps@gmail.com>
2024-03-21 15:20:10 +0530

    Food image classification completed

--- healthyfontend/src/Pages/FoodImage.jsx
+++ healthyfontend/src/Pages/FoodImage.jsx
@@ -1,82 +1,96 @@
-import React, { useState } from 'react'
-import Banner from '../Components/Banner/Banner'
-import './Css/FoodImage.css'
+import React, { useState } from 'react';
+import Banner from '../Components/Banner/Banner';
+import './Css/FoodImage.css';
 import { useDispatch, useSelector } from 'react-redux';
-
+import { setImage } from '../feature/foodImageSlice';
+import { API_BASE_URL } from '../config/config';
+import axios from 'axios';
+import ReactMarkdown from 'react-markdown';
+import { ClipLoader } from "react-spinners";
+import { ToastContainer, toast } from 'react-toastify';
 const FoodImage = (props) => {
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+  const [responseData, setResponseData] = useState(null);
 
-  const [image, setImage] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null); // Initialize as null
 
-  //Image Uploading Function
   const handleImageUpload = (event) => {
-    const selectedImage = event.target.files[0];
+    const image = event.target.files[0];
+    setSelectedImage(image);
+  };
 
-    const reader = new FileReader();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
 
-    reader.onload = () => {
-      if (reader.readyState === 2) {
-        setImage(reader.result);
-      }
-    };
-    if (selectedImage) {
-      reader.readAsDataURL(selectedImage);
+    if (!selectedImage) {
+      // Display toast message
+      toast.error("Please select an image.", {
+
+        autoClose: 2000,
+        toastId: "image-error-toast",
+        position: "top-center",
+
+      });
+      setLoading(false); // Set loading to false since no image is selected
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('image', selectedImage);
+
+    try {
+      const token = localStorage.getItem('token');
+      const response = await axios.post(
+        `${API_BASE_URL}/service/foodimageclassification/`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${token}`,
+          }
+        }
+      );
+      console.log(response.data);
+      setResponseData(response.data);
+
+    } catch (error) {
+      // Handle error here
+      console.error("Error occurred:", error);
+    } finally {
+      setLoading(false);
+
     }
   }
+
   return (
     <>
-
       <div className='foodimage-main-container'>
         <div className="banner-container">
           <Banner text={props.text} />
-
         </div>
-        <div className="foodimge">
+        <div className="foodimage">
           <h1>Food Image Classification</h1>
           <input type="file" accept="image/*" onChange={handleImageUpload} />
-          {image && (
+          <div>
+            <button onClick={handleSubmit}>Submit</button>
 
-            <div className="uploaded-image-container">
+          </div>
+        </div>
+        <div className="response-container">
+          <ToastContainer />
+          {loading ? (<div className="loading-animation">
+            <ClipLoader color={"#E0FFDE"} loading={loading} size={30} />
+          </div>) : (<div className='response-result'>
+            {responseData ? <ReactMarkdown className="recipe-markdown">{responseData.response}</ReactMarkdown> : null}
 
-              <img src={image} alt="uploaded" className='uploaded-image' />
-              
-              <div className="nutriations">
-                <h1>Food Name</h1>
+          </div>)}
 
-                <table className="nutrition-table">
-                  <tr>
-                    <th>Nutritions</th>
-                    <th>Value</th>
-                  </tr>
-                  <tr>
-                    <td>Protein</td>
-                    <td>20g</td>
-                  </tr>
-                  <tr>
-                    <td>Fat</td>
-                    <td>10g</td>
-                  </tr>
-                  <tr>
-                    <td>Carbohydrates</td>
-                    <td>30g</td>
-                  </tr>
-                  <tr>
-                    <td>Fiber</td>
-                    <td>5g</td>
-                  </tr>
-
-                </table>
-              </div>
-
-
-            </div>
-          )}
         </div>
       </div>
-
-
     </>
-  )
-}
+  );
+};
 
-export default FoodImage
+export default FoodImage;
--- healthyfontend/src/RequireAuth.jsx
+++ healthyfontend/src/RequireAuth.jsx
@@ -1,11 +1,18 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 
 
 const RequireAuth = ({ children }) => {
   const isAuthenticated = useSelector((state) => state.loginSignup.isAuthenticated);
   const location = useLocation();
+  // const [loading,setLoading] = useState(true)
 
+  
+  // if (loading){
+  //   return <div>loading...</div>
+  // }
+ 
   if (!isAuthenticated) {
     // Redirect to login if not authenticated
     return <Navigate to="/login" replace state={{ from: location }} />;
